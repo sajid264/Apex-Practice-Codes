e8a96d808a1d3f51d90b6106b5ce161911dab8d1
sajid264 <116247131+sajid264@users.noreply.github.com>
2025-08-24 19:15:10 +0600

    Added code 2 of Regions LAB

--- P4 -Working With Pages/Code 03.sql
+++ P4 -Working With Pages/Code 03.sql
@@ -19,3 +19,42 @@ WHERE
 <b>Quantity: </b> &QUANTITY. <br>
 <b>Unit Price: </b> &UNIT_PRICE.
 
+
+<b>Unit Price: &Unit_price.</b><br>
+<b>Subtotal: &SUBTOTAL.</b>
+
+SELECT
+    SEQ_ID              ITEM,
+    P.PRODUCT_IMAGE,
+    P.PRODUCT_ID,
+    P.PRODUCT_NAME      NAME,
+    P.UNIT_PRICE,
+    N002                QUANTITY,
+    P.UNIT_PRICE * N002 SUBTOTAL,
+    B.BRAND
+FROM
+    APEX_COLLECTIONS    A,
+    PRODUCTS            P,
+    JSON_TABLE ( P.PRODUCT_DETAILS, '$'
+        COLUMNS (
+            BRAND VARCHAR2 ( 4000 ) PATH '$.brand'
+        )
+    )                   B
+WHERE
+    COLLECTION_NAME = 'PRODUCTS'
+AND P.PRODUCT_ID = A.N001
+
+
+
+
+SELECT
+    M.RATING,
+    M.REVIEW
+FROM
+    PRODUCTS P,
+    PRODUCT_REVIEWS M
+WHERE
+    P.PRODUCT_NAME = M.PRODUCT_NAME
+    AND P.PRODUCT_ID = :P18_PRODUCT_ID
+ORDER BY
+    M.RATING DESC

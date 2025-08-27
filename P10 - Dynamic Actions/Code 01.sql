9270059c09e78a30b5e36f0a54b79278be82013d
sajid264 <116247131+sajid264@users.noreply.github.com>
2025-08-27 20:23:26 +0600

    Added las of P10

--- P10 - Dynamic Actions/Code 01.sql
+++ P10 - Dynamic Actions/Code 01.sql
@@ -18,3 +18,18 @@ apex.jQuery(".js-shopping-cart-item .t-Icon").removeClass('fa-cart-full').addCla
 
 RETURN manage_orders.product_exists(p_product => :P18_PRODUCT_ID);
 
+BEGIN 
+IF 
+    manage_orders.product_exists(p_product => :P18_PRODUCT_ID) = 0 
+    THEN manage_orders.add_product (p_product => :P18_PRODUCT_ID, p_quantity => :P18_QUANTITY); 
+END IF;
+:P18_ACTION := 'ADD'; 
+END;
+
+BEGIN IF manage_orders.product_exists(p_product => :P18_PRODUCT_ID) > 0 
+THEN manage_orders.remove_product(p_product => :P18_PRODUCT_ID); manage_orders.add_product (p_product => :P18_PRODUCT_ID, p_quantity => :P18_QUANTITY); 
+END IF; :P18_ACTION := 'EDIT'; 
+END;
+
+
+BEGIN IF manage_orders.product_exists(p_product => :P18_PRODUCT_ID) > 0 THEN manage_orders.remove_product(p_product => :P18_PRODUCT_ID); END IF; :P18_ACTION := 'DELETE'; END;
